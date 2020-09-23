2130c8df162b146e0681d23b07c1db1294daf74c
kazz966 <71771229+kazz966@users.noreply.github.com>
2020-09-24 04:05:09 +0900

    Add files via upload

---
+++ Add_Shift_Channels_Red.jsx
@@ -0,0 +1,4 @@
+﻿var actComp = app.project.activeItem;
+var actLy = actComp.selectedLayers;
+var FX = app.project.activeItem.selectedLayers[0]("Effects").addProperty("ADBE Shift Channels");
+FX.property(1).setValue(2);
